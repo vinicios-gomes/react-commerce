d61a662dcbadfcdebde81b8d72b9a5140842f502
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-14 17:55:40 -0300

    add roboto font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,11 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
           <title>React commerce</title>
         </Head>
         <body>
