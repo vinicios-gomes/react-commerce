fce24534da05cfaee2ece8e19360d038dc5419af
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-14 23:54:18 -0300

    add header to render for all pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <h1>React Commerce</h1>
       <Head>
         <title>Inicio | React Commerce</title>
       </Head>
