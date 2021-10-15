4adba0390a271e3500c6d38e25498160f172392a
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-15 02:25:22 -0300

    add structure to ProductCart

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,9 +1,27 @@
 import styles from "./styles.module.scss";
+import { IProduct } from "../../pages/index";
+import { formatPrice } from "../../utils/formaters";
+import { MdAddShoppingCart, MdShoppingCart } from "react-icons/md";
+import Loader from "react-loader-spinner";
 
-export function ProductCart() {
+export function ProductCart({ name, price, image, loading }: IProduct) {
   return (
     <div className={styles.productCartContainer}>
-      <h1>ProductCart</h1>
+      <img src={image} alt={name} />
+      <header>
+        <h1>{name}</h1>
+        <span>{formatPrice(price)}</span>
+        <button>
+          {loading ? (
+            <Loader type="Oval" color="#38b2ac" />
+          ) : (
+            <div>
+              <MdShoppingCart size={16} color="#38b2ac" />
+            </div>
+          )}
+          <span>Add to Cart</span>
+        </button>
+      </header>
     </div>
   );
 }
