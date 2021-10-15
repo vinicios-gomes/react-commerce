455d59b19e367403124836e2258f856e011982cf
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-15 02:25:48 -0300

    grid for list products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,38 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
+import { ProductCart } from "../components/ProductCard";
 import { getAllProducts } from "../services/api";
+import styles from "./home.module.scss";
 
-interface IProduct {
-  id: String;
-  name: String;
-  price: String;
-  image: String;
-  stock: Number;
-  createdAt: String;
+export interface IProduct {
+  id: string;
+  name: string;
+  price: string;
+  image: string;
+  stock: number;
+  createdAt: string;
 }
 
-export default function Home({ data }: IProduct[]) {
-  console.info(data);
+export default function Home({ data }) {
   return (
     <>
       <Head>
         <title>Inicio | React Commerce</title>
       </Head>
+
+      <div className={styles.listProducts}>
+        {data.map((product: IProduct) => (
+          <ProductCart
+            key={product.id}
+            name={product.name}
+            price={product.price}
+            image={product.image}
+            id={product.id}
+            stock={product.stock}
+            createdAt={product.createdAt}
+          />
+        ))}
+      </div>
     </>
   );
 }
