8069b0bb610d28a2f57c0d06bd95a3a918b6a7df
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-15 01:29:03 -0300

    add productCard component

---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export function ProductCart() {
+  return (
+    <div className={styles.productCartContainer}>
+      <h1>ProductCart</h1>
+    </div>
+  );
+}
