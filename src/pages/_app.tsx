7a00b67ba4d882b3d87e1a9cc439ba227b7c4ca4
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-15 01:40:49 -0300

    add cart context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { CartProvider } from "../context/CartContext";
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <CartProvider>
+        <Header />
+        <Component {...pageProps} />
+      </CartProvider>
     </>
   );
 }
