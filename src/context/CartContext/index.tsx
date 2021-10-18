e967b35c853c5d448fc205d82d5cd17e7fdda6b1
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-18 13:37:28 -0300

    add functions to add, remove and clear items to cart

--- src/context/CartContext/index.tsx
+++ src/context/CartContext/index.tsx
@@ -1,11 +1,36 @@
 import React, { createContext, useState } from "react";
+import { IProduct } from "../../pages";
 
 export const CartContext = createContext({});
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+
+  function handleAddProductToCart(product: IProduct) {
+    setCart([...cart, product]);
+  }
+
+  function handleRemoveProductFromCart(product: IProduct) {
+    const filteredCart = cart.filter(
+      (cartItem: IProduct) => cartItem.id !== product.id
+    );
+
+    setCart(filteredCart);
+  }
+
+  function clearCart() {
+    setCart([]);
+  }
+
   return (
-    <CartContext.Provider value={{ cart, setCart }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        handleAddProductToCart,
+        handleRemoveProductFromCart,
+        clearCart,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
