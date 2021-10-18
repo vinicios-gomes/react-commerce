4b06afa9c78eee6791b7aa3af70a28f1a1f0a17d
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-18 19:51:20 -0300

    get item by id

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,18 @@
+import { IProduct } from "../pages";
+
 export const getAllProducts = async () => {
   const data = await fetch(
     "https://5d6da1df777f670014036125.mockapi.io/api/v1/product"
   );
   return data.json();
+};
+export const getProductById = async (id) => {
+  const data = await fetch(
+    "https://5d6da1df777f670014036125.mockapi.io/api/v1/product"
+  );
+  const serializedData: IProduct[] = await data.json();
+
+  const filteredProduct = serializedData.filter((product) => product.id === id);
+
+  return filteredProduct;
 };
