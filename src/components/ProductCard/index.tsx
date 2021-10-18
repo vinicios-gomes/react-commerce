8eb667a6c72f84a65a18d1eb0e7a5ee833a77800
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-18 18:20:03 -0300

    fix props

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -2,23 +2,22 @@ import styles from "./styles.module.scss";
 import { IProduct } from "../../pages/index";
 import { formatPrice } from "../../utils/formaters";
 import { MdAddShoppingCart, MdShoppingCart } from "react-icons/md";
-import Loader from "react-loader-spinner";
+import { useContext } from "react";
+import { CartContext, CartProvider } from "../../context/CartContext";
 
-export function ProductCart({ name, price, image, loading }: IProduct) {
+export function ProductCart(product: IProduct) {
   return (
     <div className={styles.productCartContainer}>
-      <img src={image} alt={name} />
+      <img src={product.image} alt={product.name} />
       <header>
-        <h1>{name}</h1>
-        <span>{formatPrice(price)}</span>
+        <strong>{product.name}</strong>
+
+        <span>{formatPrice(product.price)}</span>
+
         <button>
-          {loading ? (
-            <Loader type="Oval" color="#38b2ac" />
-          ) : (
-            <div>
-              <MdShoppingCart size={16} color="#38b2ac" />
-            </div>
-          )}
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
           <span>Add to Cart</span>
         </button>
       </header>
