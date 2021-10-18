bbabab149c474521fe606fa1a9aefba71372dcee
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-18 17:25:47 -0300

    route to / and cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,26 @@
 import styles from "./styles.module.scss";
+import Link from "next/link";
 import { MdShoppingCart } from "react-icons/md";
 
 export function Header() {
+  function handleSidebar(status) {}
+
   return (
     <header className={styles.headerContainer}>
-      <img src="/images/logo.png" alt="React Commerce" />
+      <Link href="/">
+        <img src="/images/logo.png" alt="React Commerce" />
+      </Link>
 
-      <div className={styles.headerCart}>
-        <div>
-          <strong>My cart</strong>
-          <span>0 Products</span>
+      <Link href="/cart">
+        <div className={styles.headerCart}>
+          <div>
+            <strong>My cart</strong>
+            <span>0 Products</span>
+          </div>
+          <MdShoppingCart size={38} color="#000" />
+          <span>0</span>
         </div>
-        <MdShoppingCart size={38} color="#000" />
-        <span>0</span>
-      </div>
+      </Link>
     </header>
   );
 }
