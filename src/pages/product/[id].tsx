cd9c12fc786189042916755dce6d3ce0e1cc76b1
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-18 21:18:57 -0300

    page of product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,8 @@
 import { GetServerSideProps } from "next";
 import { MdAddShoppingCart } from "react-icons/md";
 import { IProduct } from "..";
-import { getAllProducts, getProductById } from "../../services/api";
+import { getProductById } from "../../services/api";
+import { formatPrice } from "../../utils/formaters";
 import style from "./product.module.scss";
 
 interface IProductProps {
@@ -11,22 +12,27 @@ interface IProductProps {
 export default function Product({ products }: IProductProps) {
   return (
     <>
-      {products.map((product) => (
-        <div className={style.product}>
-          <img src={product.image} alt={product.name} />
-          <div>
-            <h1>{product.name}</h1>
-            <span>{product.stock}</span>
-            <strong>{product.price}</strong>
-            <button>
-              <div>
-                <MdAddShoppingCart size={16} color="#fff" />
-              </div>
-              <span>Add to Cart</span>
-            </button>
+      <div className={style.container}>
+        {products.map((product) => (
+          <div className={style.product}>
+            <img src={product.image} alt={product.name} />
+
+            <div className={style.description}>
+              <h1>{product.name}</h1>
+              <span>Stock: {product.stock}</span>
+              <span>
+                Price: <strong>{formatPrice(product.price)}</strong>
+              </span>
+              <button>
+                <div>
+                  <MdAddShoppingCart size={16} color="#fff" />
+                </div>
+                <span>Add to Cart</span>
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </>
   );
 }
