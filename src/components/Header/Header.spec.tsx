be5c2bd66107044e3e6fdca76f080d0a0ce03708
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-20 19:27:21 -0300

    check if render correctly amount of products in cart on header

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -15,4 +15,39 @@ describe("Header Component", () => {
     expect(screen.getByAltText("React Commerce")).toBeInTheDocument();
     expect(screen.getByText("My cart")).toBeInTheDocument();
   });
+
+  it("Should be showing 0 if there is nothing in the cart", () => {
+    const { getByText } = render(
+      <CartStore>
+        <Header />
+      </CartStore>
+    );
+    expect(getByText("0")).toBeInTheDocument();
+  });
+
+  it("Should be showing the amount of the products in the cart", () => {
+    const product: IProduct = {
+      createdAt: "2021-03-03",
+      id: "1",
+      image: "https://localhost/image",
+      name: "Mock Product",
+      price: "232.00",
+      stock: 23,
+      amount: 1,
+    };
+
+    const initialState: CartGlobalState = {
+      products: [product],
+    };
+
+    render(
+      <cartContext.Provider
+        value={{ dispatch: jest.fn(), cartState: initialState }}
+      >
+        <Header />
+      </cartContext.Provider>
+    );
+
+    expect(screen.getByText("1")).toBeInTheDocument();
+  });
 });
