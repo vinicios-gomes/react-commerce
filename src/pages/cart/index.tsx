85c1d9af4aea38baf4df1b0130176e8d95445eae
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-25 09:50:16 -0300

    check if have products in cart, if dont have, shoe empty content

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import Link from "next/link";
 import {
   MdAddCircleOutline,
   MdDelete,
@@ -48,84 +49,99 @@ export default function Cart() {
   return (
     <>
       <div className={style.cartContainer}>
-        <table data-cy="product-table">
-          <thead>
-            <tr>
-              <th />
-              <th>PRODUCT</th>
-              <th>AMOUNT</th>
-              <th>SUBTOTAL</th>
-            </tr>
-          </thead>
-          <tbody>
-            {sortProductList.map((product) => (
-              <tr data-cy="product-table-list-item" key={product.id}>
-                <td>
-                  <figure>
-                    <img src={product.image}></img>
-                  </figure>
-                </td>
-                <td>
-                  <strong>{product.name}</strong>
-                  <span>{formatPrice(product.price)}</span>
-                </td>
-                <td>
-                  <div>
-                    <button
-                      type="button"
-                      data-testid="cart-decrement-product"
-                      onClick={() =>
-                        decreaseQtdProduct(product.id, product.amount)
-                      }
-                    >
-                      <MdRemoveCircleOutline size={20} color="#1c5d99" />
-                    </button>
-                    <input
-                      type="text"
-                      readOnly
-                      data-testid="product-amount"
-                      value={product.amount}
-                    />
-                    <button
-                      type="button"
-                      data-testid="cart-increment-product"
-                      onClick={() =>
-                        increaseQtdProduct(product.id, product.amount)
-                      }
-                    >
-                      <MdAddCircleOutline size={20} color="#1c5d99" />
-                    </button>
-                  </div>
-                </td>
-                <td>
-                  <strong data-cy="cart-subtotal">
-                    {formatSubTotal(product.price, product.amount)}
-                  </strong>
-                </td>
-                <td>
-                  <button
-                    onClick={() => removeItemOfCart(product.id)}
-                    type="button"
-                  >
-                    <MdDelete size={20} color="#1c5d99" />
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        {sortProductList.length ? (
+          <>
+            <table data-cy="product-table">
+              <thead>
+                <tr>
+                  <th />
+                  <th>PRODUCT</th>
+                  <th>AMOUNT</th>
+                  <th>SUBTOTAL</th>
+                </tr>
+              </thead>
+              <tbody>
+                {sortProductList.map((product) => (
+                  <tr data-cy="product-table-list-item" key={product.id}>
+                    <td>
+                      <figure>
+                        <img src={product.image}></img>
+                      </figure>
+                    </td>
+                    <td>
+                      <strong>{product.name}</strong>
+                      <span>{formatPrice(product.price)}</span>
+                    </td>
+                    <td>
+                      <div>
+                        <button
+                          type="button"
+                          data-testid="cart-decrement-product"
+                          onClick={() =>
+                            decreaseQtdProduct(product.id, product.amount)
+                          }
+                        >
+                          <MdRemoveCircleOutline size={20} color="#1c5d99" />
+                        </button>
+                        <input
+                          type="text"
+                          readOnly
+                          data-testid="product-amount"
+                          value={product.amount}
+                        />
+                        <button
+                          type="button"
+                          data-testid="cart-increment-product"
+                          onClick={() =>
+                            increaseQtdProduct(product.id, product.amount)
+                          }
+                        >
+                          <MdAddCircleOutline size={20} color="#1c5d99" />
+                        </button>
+                      </div>
+                    </td>
+                    <td>
+                      <strong data-cy="cart-subtotal">
+                        {formatSubTotal(product.price, product.amount)}
+                      </strong>
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => removeItemOfCart(product.id)}
+                        type="button"
+                      >
+                        <MdDelete size={20} color="#1c5d99" />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
 
-        <footer className={style.footer}>
-          <button data-cy="cart-continue-checkout" type="submit">
-            Continue to checkout
-          </button>
-          <div>
-            <span>Total: </span>
-            <strong data-testid="cart-total">
-              {formatPrice(totalCartPrice)}
-            </strong>
+            <footer className={style.footer}>
+              <button data-cy="cart-continue-checkout" type="submit">
+                Continue to checkout
+              </button>
+              <div>
+                <span>Total: </span>
+                <strong data-testid="cart-total">
+                  {formatPrice(totalCartPrice)}
+                </strong>
+              </div>
+            </footer>
+          </>
+        ) : (
+          <div className={style.emptyContent}>
+            <h1>Oops...</h1>
+            <span>
+              Looks like you have no items in your cart. Let's start the
+              purchases ?!
+            </span>
+            <Link href="/">
+              <a>Start shopping!</a>
+            </Link>
           </div>
-        </footer>
+        )}
       </div>
     </>
   );
